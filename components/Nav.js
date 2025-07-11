bea4797d1b639a9ab7c9c8cd094f4f708ac8f124
yeshu2004 <playeryeshu@gmail.com>
2025-07-11 22:58:55 +0530

    added-nav-animation

--- components/Nav.js
+++ components/Nav.js
@@ -38,6 +38,21 @@ export default function Nav() {
     },
   };
 
+  const bigTextAnimation = (delay) => ({
+    initial: { y: 50 },
+    animate: { y: 0, transition: { delay, ease: [0.77, 0, 0.175, 1], duration: 1.5 } },
+  });
+
+  const smallTextAnimation = (delay) => ({
+    initial: { y: 25 },
+    animate: { y: 0, transition: { delay, ease: [0.77, 0, 0.175, 1], duration: 1.5 } },
+  });
+
+  const underlineAnimation = (delay) => ({
+    initial: { width: 0 },
+    animate: { width: "100%", transition: { delay, duration: 1, ease: "easeInOut" } },
+  });
+
   return (
     <>
       <div className="text-xs flex items-center justify-between font-medium fixed w-full px-4 z-50 bg-transparent">
@@ -87,100 +102,152 @@ export default function Nav() {
             <div className="w-full pt-20 flex-1">
               <div className="w-full px-4">
                 <div className="w-full">
-                  <h2 className="pb-2 text-zinc-500 font-medium">
-                    (Navigation)
-                  </h2>
-                  <div className="w-full bg-[#0e0e0e] h-[1px]"></div>
+                  <div className="overflow-hidden">
+                    <motion.h2
+                      {...smallTextAnimation(1)}
+                      className="pb-2 text-zinc-500 font-medium"
+                    >
+                      (Navigation)
+                    </motion.h2>
+                  </div>
+                  <motion.div
+                    {...underlineAnimation(1.05)}
+                    className="w-full bg-[#0e0e0e] h-[1px]"
+                  ></motion.div>
                 </div>
                 <div className="w-full">
                   <div className="flex pb-2 items-center text-4xl justify-between">
-                    <Link
-                      href="/"
-                      className={`text-4xl ${
-                        pathname === "/" ? "text-[#0e0e0e]" : "text-zinc-500"
-                      }`}
-                    >
-                      Home
-                    </Link>
-                    <div
-                      className={`text-4xl ${
-                        pathname === "/" ? "text-[#0e0e0e]" : "text-zinc-500"
-                      }`}
-                    >
-                      01
+                    <div className="overflow-hidden">
+                      <motion.div {...bigTextAnimation(1.1)}>
+                        <Link
+                          href="/"
+                          className={`text-4xl ${
+                            pathname === "/" ? "text-[#0e0e0e]" : "text-zinc-500"
+                          }`}
+                        >
+                          Home
+                        </Link>
+                      </motion.div>
+                    </div>
+                    <div className="overflow-hidden">
+                      <motion.div
+                        {...bigTextAnimation(1.2)}
+                        className={`text-4xl ${
+                          pathname === "/" ? "text-[#0e0e0e]" : "text-zinc-500"
+                        }`}
+                      >
+                        01
+                      </motion.div>
                     </div>
                   </div>
-                  <div className="w-full bg-[#0e0e0e] h-[1px]"></div>
+                  <motion.div
+                    {...underlineAnimation(1.25)}
+                    className="w-full bg-[#0e0e0e] h-[1px]"
+                  ></motion.div>
                 </div>
                 <div className="w-full">
                   <div className="flex pb-2 items-center text-4xl justify-between">
-                    <Link
-                      href="/events"
-                      className={`text-4xl ${
-                        pathname === "/events"
-                          ? "text-[#0e0e0e]"
-                          : "text-zinc-500"
-                      }`}
-                    >
-                      Events
-                    </Link>
-                    <div
-                      className={`text-4xl ${
-                        pathname === "/events"
-                          ? "text-[#0e0e0e]"
-                          : "text-zinc-500"
-                      }`}
-                    >
-                      02
+                    <div className="overflow-hidden">
+                      <motion.div {...bigTextAnimation(1.3)}>
+                        <Link
+                          href="/events"
+                          className={`text-4xl ${
+                            pathname === "/events"
+                              ? "text-[#0e0e0e]"
+                              : "text-zinc-500"
+                          }`}
+                        >
+                          Events
+                        </Link>
+                      </motion.div>
+                    </div>
+                    <div className="overflow-hidden">
+                      <motion.div
+                        {...bigTextAnimation(1.4)}
+                        className={`text-4xl ${
+                          pathname === "/events"
+                            ? "text-[#0e0e0e]"
+                            : "text-zinc-500"
+                        }`}
+                      >
+                        02
+                      </motion.div>
                     </div>
                   </div>
-                  <div className="w-full bg-[#0e0e0e] h-[1px]"></div>
+                  <motion.div
+                    {...underlineAnimation(1.45)}
+                    className="w-full bg-[#0e0e0e] h-[1px]"
+                  ></motion.div>
                 </div>
                 <div className="w-full">
                   <div className="flex pb-2 items-center text-4xl justify-between">
-                    <Link
-                      href="/members"
-                      className={`text-4xl ${
-                        pathname === "/members"
-                          ? "text-[#0e0e0e]"
-                          : "text-zinc-500"
-                      }`}
-                    >
-                      Members
-                    </Link>
-                    <div
-                      className={`text-4xl ${
-                        pathname === "/members"
-                          ? "text-[#0e0e0e]"
-                          : "text-zinc-500"
-                      }`}
-                    >
-                      03
+                    <div className="overflow-hidden">
+                      <motion.div {...bigTextAnimation(1.5)}>
+                        <Link
+                          href="/members"
+                          className={`text-4xl ${
+                            pathname === "/members"
+                              ? "text-[#0e0e0e]"
+                              : "text-zinc-500"
+                          }`}
+                        >
+                          Members
+                        </Link>
+                      </motion.div>
+                    </div>
+                    <div className="overflow-hidden">
+                      <motion.div
+                        {...bigTextAnimation(1.6)}
+                        className={`text-4xl ${
+                          pathname === "/members"
+                            ? "text-[#0e0e0e]"
+                            : "text-zinc-500"
+                        }`}
+                      >
+                        03
+                      </motion.div>
                     </div>
                   </div>
-                  <div className="w-full bg-[#0e0e0e] h-[1px]"></div>
+                  <motion.div
+                    {...underlineAnimation(1.65)}
+                    className="w-full bg-[#0e0e0e] h-[1px]"
+                  ></motion.div>
                 </div>
               </div>
             </div>
 
             <div className="w-full px-4 pb-4">
-              <div className="flex items-center gap-1">
-                <Link
-                  href="https://www.instagram.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1"
-                >
-                  Instagram
-                  <GoArrowUpRight />
-                </Link>
+              <div className="overflow-hidden">
+                <motion.div {...smallTextAnimation(1.7)}>
+                  <Link
+                    href="https://www.instagram.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1"
+                  >
+                    Instagram
+                    <GoArrowUpRight />
+                  </Link>
+                </motion.div>
               </div>
               <div className="flex items-center justify-between text-sm mt-2">
-                <div className="flex flex-col leading-none">
-                <h1>Collectible Furniture</h1> <h1>and Objects</h1>
+                <div className="overflow-hidden">
+                  <motion.div
+                    {...smallTextAnimation(1.8)}
+                    className="flex flex-col leading-none"
+                  >
+                    <h1>Collectible Furniture</h1>
+                    <h1>and Objects</h1>
+                  </motion.div>
                 </div>
-                <div className="flex flex-col leading-none">
-                <h1>Designed by</h1><h1> Ux Club, Vit Bhopal</h1>
+                <div className="overflow-hidden">
+                  <motion.div
+                    {...smallTextAnimation(1.8)}
+                    className="flex flex-col leading-none"
+                  >
+                    <h1>Designed by</h1>
+                    <h1>UX Club, VIT Bhopal</h1>
+                  </motion.div>
                 </div>
               </div>
             </div>
